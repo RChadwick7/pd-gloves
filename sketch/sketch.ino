db68027679501f8876242be9c254f4ffb85791fd
HackyDev <hackydev@gmail.com>
2022-12-13 22:56:24 +0600

    script works now

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,7 +1,7 @@
 // PROGRAM SETTINGS
 unsigned long burstDurationMicrosecs = 100000;
 unsigned long pauseDurationMicrosecs = 66700;
-int burstFrequencyPercent = 100; // motor speed for a pair of fingers being actively stimulated (value from 0 to 100)
+int burstFrequencyPercent = 100; // motor speed for a pair of fingers actively stimulated (value from 0 to 100)
 int restFrequencyPercent = 0; // motor speed for the fingers not being actively stimulated (value from 0 to 100)
 int pauseFrequencyPercent = 0; // motor speed for all fingers during pause (value from 0 to 100)
 
@@ -17,6 +17,8 @@ enum State {
   PAUSE_STARTED,
   PAUSE_DONE
 };
+
+const char* stateStr[] = { "OFF", "CYCLE_STARTED", "CYCLE_DONE", "STEP_STARTED", "STEP_DONE", "VIBRATION_STARTED", "VIBRATION_DONE", "PAUSE_STARTED", "PAUSE_DONE" };
 
 State currentState = OFF;
 
@@ -86,16 +88,31 @@ int burstFrequency = map(burstFrequencyPercent, 0, 100, 0, 255);
 int restFrequency = map(restFrequencyPercent, 0, 100, 0, 255);
 int pauseFrequency = map(pauseFrequencyPercent, 0, 100, 0, 255);
 Timer timer = Timer();
+bool development = true;
 
 void setup() {
+  Serial.begin(9600);
+  Serial.println(currentState);
   for (int i = 0; i < 4; i++) {
     pinMode(fingerPins[i], OUTPUT);
-    digitalWrite(fingerPins[i], LOW);
+    analogWrite(fingerPins[i], 0);
   }
 }
 
 void loop() {
+  State beforeUpdateState = currentState;
+
   updateCurrentState();
+
+  // DEBUG
+  if (development == true) {
+    if (currentState != beforeUpdateState) {
+      Serial.print("NEW STATE: ");
+      Serial.println(stateStr[currentState]);
+    }
+  }
+
+
   if (currentState == CYCLE_STARTED) {
     currentStep = -1;
   } else if (currentState == STEP_STARTED) {
@@ -116,9 +133,9 @@ void updateCurrentState () {
     currentState = CYCLE_STARTED;
   } else if (currentState == CYCLE_DONE) {
     currentState = CYCLE_STARTED;
-  } else if (currentState = CYCLE_STARTED) {
+  } else if (currentState == CYCLE_STARTED) {
     currentState = STEP_STARTED;
-  } else if (currentState = STEP_STARTED) {
+  } else if (currentState == STEP_STARTED) {
     currentState = VIBRATION_STARTED;
     timer.start(burstDurationMicrosecs);
   } else if (currentState == VIBRATION_STARTED) {
@@ -135,16 +152,20 @@ void updateCurrentState () {
   } else if (currentState == PAUSE_DONE) {
     currentState = STEP_DONE;
   } else if (currentState == STEP_DONE) {
-    currentState = CYCLE_DONE;
+    if (currentStep < 32) {
+      currentState = STEP_STARTED;
+    } else {
+      currentState = CYCLE_DONE;  
+    }
   }
 }
 
 void startVibration (int pattern[4]) {
   for (int i = 0; i < 4; i++) {
     if (pattern[i]) {
-      digitalWrite(pattern[i], burstFrequency);
+      analogWrite(fingerPins[i], burstFrequency);
     } else {
-      digitalWrite(pattern[i], restFrequency);
+      analogWrite(fingerPins[i], restFrequency);
     }
   }
 }
@@ -163,6 +184,6 @@ void stopPause () {
 
 void toggleFingerPins (int value) {
   for (int i = 0; i < 4; i++) {
-    digitalWrite(fingerPins[i], value);
+    analogWrite(fingerPins[i], value);
   }
 }
