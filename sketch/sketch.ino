aa925e3d3b055fa95c88cd26804efe1c7a434857
HackyDev <hackydev@gmail.com>
2022-12-13 21:41:54 +0600

    improved comments

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,7 +1,7 @@
 // PROGRAM SETTINGS
 unsigned long burstDurationMicrosecs = 100000;
 unsigned long pauseDurationMicrosecs = 66700;
-int burstFrequencyPercent = 100; // motor speed for a pair of fingers actively stimulated (value from 0 to 100)
+int burstFrequencyPercent = 100; // motor speed for a pair of fingers being actively stimulated (value from 0 to 100)
 int restFrequencyPercent = 0; // motor speed for the fingers not being actively stimulated (value from 0 to 100)
 int pauseFrequencyPercent = 0; // motor speed for all fingers during pause (value from 0 to 100)
 
